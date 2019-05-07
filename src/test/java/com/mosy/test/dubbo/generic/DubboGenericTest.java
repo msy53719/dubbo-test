ade00537f6951adf02ddae12c19a31b1f44a3b11
mosy <479979298@qq.com>
2019-05-07 15:13:09 +0800

    delete jar

--- src/test/java/com/mosy/test/dubbo/generic/DubboGenericTest.java
+++ src/test/java/com/mosy/test/dubbo/generic/DubboGenericTest.java
@@ -1,6 +1,5 @@
 package com.mosy.test.dubbo.generic;
 
-import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,45 +16,50 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class DubboGenericTest {
 
-	@org.junit.Test
-	public void genericTest() {
-
-		System.out.println("111");
-	}
-
 	public static void main(String[] args) {
 
 		ApplicationConfig application = new ApplicationConfig();
 		application.setName("dubbo-server");
-
 		RegistryConfig registry = new RegistryConfig();
 		registry.setAddress("zookeeper://127.0.0.1:2181");
-
 		ReferenceConfig<GenericService> reference = new ReferenceConfig<GenericService>();
 		reference.setApplication(application);
-		//reference.;
 		reference.setRegistry(registry);
 		reference.setInterface("com.project.framework.service.UserService");
-//		try {
-//			Class<?> cls = Class.forName("com.project.framework.service.UserService");
-//			reference.setInterface(cls);
-//			Method[] method = cls.getMethods();
-//			for (Method method2 : method) {
-//				System.out.println(method2.getName());
-//			}
-//		} catch (ClassNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		// reference.setInterface(cls);
-		// reference.set
 		reference.setGeneric(true);
-		//reference.get
 		ReferenceConfigCache cache = ReferenceConfigCache.getCache();
-		
 		GenericService genericService = cache.get(reference);
+		Method[] cls=	reference.getInterfaceClass().getMethods();
+		for (Method  me : cls) {
+			System.out.println(me.getName());
+		}
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("name", "test001");
+		map.put("phone", "13211111111");
+		map.put("sex", "女");
+		map.put("age", "19");
+		map.put("address", "北京市西城区三里河");
+
+		Object result = genericService.$invoke("reginseter", new String[] { "com.project.framework.dto.UserDto" },
+				new Object[] { map });
+		System.out.println(result);
+		log.debug(JSON.toJSONString(result));
 		
-	
+		
+//		try {
+//		Class<?> cls = Class.forName("com.project.framework.service.UserService");
+//		reference.setInterface(cls);
+//		Method[] method = cls.getMethods();
+//		for (Method method2 : method) {
+//			System.out.println(method2.getName());
+//		}
+//	} catch (ClassNotFoundException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+	// reference.setInterface(cls);
+	// reference.set
+		//reference.get
 		//cls.getClass().getName();
 		//genericService.getClass().get
 		// GenericService genericService1 =reference.get();
@@ -76,27 +80,17 @@ public class DubboGenericTest {
 		// "reginseter", new String[] { "java.lang.String" },
 		// new Object[] { "1", "2", "3", "4", "5" });
 		// //System.out.println(result);
-		
-		Method[] cls=	reference.getInterfaceClass().getMethods();
-		for (Method  me : cls) {
-			System.out.println(me.getName());
-		}
-
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("name", "test001");
-		map.put("phone", "13211111111");
-		map.put("sex", "女");
-		map.put("age", "19");
-		map.put("address", "北京市西城区三里河");
 		// Method[]
 		// method=genericService.getClass().getMethod("com.project.framework.service.UserService",
 		// Object);
-		Object result = genericService.$invoke("reginseter", new String[] { "com.project.framework.dto.UserDto" },
-				new Object[] { map });
-		System.out.println(result);
-		log.debug(JSON.toJSONString(result));
-		
 	
 	}
+
+	@org.junit.Test
+	public void genericTest() {
+
+		System.out.println("111");
+	}
+
 
 }
